080c386d26dde44cd3557260bca79c141379d718
kevferreras <kevferreras11@gmail.com>
2021-01-30 14:07:57 -0500

    hello world program and test

---
+++ Hello_Kevin_Ferreras.py
@@ -0,0 +1,11 @@
+def hello():
+    return "Hello World!"
+
+def main():
+    '''Prints the message "Hello World!'''
+
+    print(hello())
+
+if __name__ == "__main__":
+    main()
+    
---
+++ Test_Hello_Kevin_Ferreras.py
@@ -0,0 +1,15 @@
+import unittest
+from Hello_Kevin_Ferreras import hello
+
+class HelloTest(unittest.TestCase):
+
+    def test_hello(self) -> None:
+        ''' Tests that the hello fundtion returns the message "Hello World!'''
+
+        message: str = hello()
+        expected_message: str = "Hello World!"
+
+        self.assertEqual(message, expected_message)
+
+if __name__ == "__main__":
+    unittest.main(exit = False, verbosity = 2)
